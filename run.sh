e6eeeb778ee3a8ec4beb51f556e9804c5771998f
root <root@localhost.localdomain>
2018-11-07 17:12:44 -0600

    run updates

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ usage(){
 	echo "	'r:name (str)' # immediate rename to a static name "
 	echo "	'R:name (str)' # delayed rename to a static name "
 	echo "	'M:key  (int)' # delayed rename to a dynamic name "
+	echo 
+	echo "Ex.: ./run.sh  -b r:[rpcd] ./main"
 	exit 1
 }
 make clean
@@ -22,16 +24,17 @@ if [ $# -ne 3 ]
     usage
 fi
 
+
 while getopts ":b:B:f" opt; do
   case $opt in
     b)
-		LD_BG=true  LD_CMD=$2 LD_PRELOAD="/root/Code/psevade/libctx.so.1.0" $3
+		LD_BG=true  LD_CMD=$2 LD_PRELOAD="${PWD}/lib/libctx.so.1" $3
       ;;
     B)
-		LD_BG="fdclose"  LD_CMD=$2 LD_PRELOAD="/root/Code/psevade/libctx.so.1.0" $3
+		LD_BG="fdclose"  LD_CMD=$2 LD_PRELOAD="${PWD}/lib/libctx.so.1" $3
       ;;
     f)
-		LD_CMD=$2 LD_PRELOAD="/root/Code/psevade/libctx.so.1.0" $3
+		LD_CMD=$2 LD_PRELOAD="${PWD}/lib/libctx.so.1" $3
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
